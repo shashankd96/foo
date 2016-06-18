65b6c26123ed0300492e3f87e09321b04f2ee952
shashankd96 <shashank.d96@gmail.com>
2016-06-18 14:36:54 +0530

    Shanky foo initial commit

---
+++ foo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2
+import time
+x = time.ctime() +" : Script - "+ __file__
+print x
+f = open(__file__, 'a')
+f.write('\n# '+x)
+f.close()
+
+
+
+# Sat Jun 18 14:26:17 2016 : Script - foo.py
+# Sat Jun 18 14:26:21 2016 : Script - foo.py
+# Sat Jun 18 14:26:22 2016 : Script - foo.py
+# Sat Jun 18 14:26:23 2016 : Script - foo.py
+
+# Sat Jun 18 14:26:34 2016 : Script - foo.py
